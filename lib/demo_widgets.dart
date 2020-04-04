e705946099c5875ff74f18fb9cc2cda1a0c2cbee
yohom <yohombao@qq.com>
2020-04-04 13:20:41 +0800

    enhance: 导出

--- lib/demo_widgets.dart
+++ lib/demo_widgets.dart
@@ -1,7 +1,7 @@
 library demo_widgets;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/button.widget.dart';
+export 'src/function_group.widget.dart';
+export 'src/funtion_item.widget.dart';
+export 'src/setting.widget.dart';
+export 'src/todo.screen.dart';
